64e2291899985118a9333ee984b09fc53b2a0af0
Mohd Faizan <ansarifaizan85742@gmail.com>
2024-08-03 17:56:03 +0530

    Project Initialization

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const page = () => {
+const Navbar = () => {
   return (
     <div>
-      Home
+      Navbar
     </div>
   )
 }
 
-export default page
+export default Navbar
